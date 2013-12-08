5afa3b533398842d603ea2851013b6e506605572
Tom Ashworth <tashworth@twitter.com>
2013-12-08 01:45:41 +0000

    Tweak the fuzzy matching.

--- app/js/ui/with_search_object.js
+++ app/js/ui/with_search_object.js
@@ -4,12 +4,20 @@ define(function (require) {
     return withSearchObject;
     function withSearchObject() {
         this.searchArray = function (arr, str) {
-            var strArr = arr.map(function (v) { return v.name + ' ' + (v.data.name || ''); });
-            var matches = fuzzy.filter(str, strArr);
+            var strArr = arr
+                .filter(function (v) { return !!v.data.name; })
+                .map(function (v) { return v.data.name + ' ';  });
+            var matches = fuzzy.filter(str, strArr, {
+                pre: '<strong>',
+                post: '</strong>'
+            });
             return matches.sort(function (a, b) {
                 return a.score - b.score;
             }).reduce(function (memo, v) {
-                memo.push(arr[v.index]);
+                memo.push({
+                    match: v,
+                    data: arr[v.index]
+                });
                 return memo;
             }, []);
         };
